924e2fd545a80ebd4872ee6954098dd1199f50c8
j521887 <554950972@qq.com>
2016-05-16 10:56:44 +0800

    Initial Commit

---
+++ VideoMediaWork1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoMediaWork1
+//
+//  Created by Des on 16/5/16.
+//  Copyright © 2016年 WJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
